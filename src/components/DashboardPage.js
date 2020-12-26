0bef69349603050e87c9486fd6edab43937b2888
Francis J <fajoaq@gmail.com>
2020-12-26 16:06:29 -0500

    refactor, connect components to store

--- src/components/DashboardPage.js
+++ src/components/DashboardPage.js
@@ -1,31 +1,13 @@
 import React from 'react';
-import { v4 as uuidv4 } from 'uuid';
 
 import AddPost from './AddPost';
 import PostList from './PostList';
 
-export class DashboardPage extends React.Component {
-  state = {
-    postList: []
-  };
-  onAddPost = () => {
-    const newPost = {
-      postTitle: 'New Post',
-      id: uuidv4()
-    };
-
-    this.setState(() => ({
-      postList: this.state.postList.concat(newPost)
-    }));
-  };
-  render() {
-    return (
-      <div>
-        <AddPost onAddPost={ this.onAddPost } />
-        <PostList postList={ this.state.postList } />
-      </div>
-    )
-  };
-};
+export const DashboardPage = () => (
+    <div>
+      <AddPost />
+      <PostList />
+    </div>
+  );
 
 export default DashboardPage;
--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,13 +1,15 @@
 import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import authReducer from '../reducers/auth';
+import PostsReducer from '../reducers/posts'
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 export default () => {
   const store = createStore(
     combineReducers({
-      auth: authReducer
+      auth: authReducer,
+      postList: PostsReducer
     }),
     composeEnhancers(applyMiddleware(thunk))
   );
