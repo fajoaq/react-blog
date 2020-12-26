a1063f4b73a7f4ab87b5210ea00f2ab6701503df
Francis J <fajoaq@gmail.com>
2020-12-26 14:34:15 -0500

    Add post button creates a new post on dashboard

--- src/components/DashboardPage.js
+++ src/components/DashboardPage.js
@@ -1,10 +1,31 @@
 import React from 'react';
+import { v4 as uuidv4 } from 'uuid';
 
+import AddPost from './AddPost';
+import PostList from './PostList';
 
-const DashboardPage = () => (
-  <div>
+export class DashboardPage extends React.Component {
+  state = {
+    postList: []
+  };
+  onAddPost = () => {
+    const newPost = {
+      postTitle: 'New Post',
+      id: uuidv4()
+    };
 
-  </div>
-);
+    this.setState(() => ({
+      postList: this.state.postList.concat(newPost)
+    }));
+  };
+  render() {
+    return (
+      <div>
+        <AddPost onAddPost={ this.onAddPost } />
+        <PostList postList={ this.state.postList } />
+      </div>
+    )
+  };
+};
 
 export default DashboardPage;
